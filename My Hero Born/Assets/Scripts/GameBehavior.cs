2cd1d6b182788f72b41f7f5a426db92c00af3cc0
Paige C <77869251+pkcody@users.noreply.github.com>
2021-02-21 13:51:26 -0700

    updated scripts

--- My Hero Born/Assets/Scripts/GameBehavior.cs
+++ My Hero Born/Assets/Scripts/GameBehavior.cs
@@ -8,15 +8,16 @@ public class GameBehavior : MonoBehaviour
     public bool showWinScreen = false;
     public string labelText = "Collect all 4 items and win your freedom!";
     public int maxItems = 4;
+    public bool showLossScreen = false;
 
     private int _itemsCollected = 0;
     public int Items
     {
         get { return _itemsCollected; }
-        set 
-        { 
+        set
+        {
             _itemsCollected = value;
-            if(_itemsCollected >= maxItems)
+            if (_itemsCollected >= maxItems)
             {
                 labelText = "You've found all the items!";
                 showWinScreen = true;
@@ -28,31 +29,51 @@ public class GameBehavior : MonoBehaviour
             }
         }
     }
-        
-    private int _playerHP = 10;
 
+    private int _playerHP = 10;
     public int HP
     {
         get { return _playerHP; }
         set
         {
             _playerHP = value;
-            Debug.LogFormat("Lives: {0}", _playerHP);
+            if (_playerHP <= 0)
+            {
+                labelText = "You want another life with that?";
+                showLossScreen = true;
+                Time.timeScale = 0;
+            }
+            else
+            {
+                labelText = "Ouch... that's got hurt.";
+            }
         }
-    } 
-        
+    }
+
+    void RestartLevel()
+    {
+        SceneManager.LoadScene(0);
+        Time.timeScale = 1.0f;
+    }
+
     void OnGUI()
     {
         GUI.Box(new Rect(20, 20, 150, 25), "Player Health:" + _playerHP);
         GUI.Box(new Rect(20, 50, 150, 25), "Items Collected:" + _itemsCollected);
-        GUI.Label(new Rect(Screen.width / 2- 100, Screen.height - 50, 300, 50), labelText);
+        GUI.Label(new Rect(Screen.width / 2 - 100, Screen.height - 50, 300, 50), labelText);
 
         if (showWinScreen)
         {
-            if (GUI.Button(new Rect(Screen.width/2 - 100, Screen.height/2 -50, 200, 100), "YOU WON!"))
+            if (GUI.Button(new Rect(Screen.width / 2 - 100, Screen.height / 2 - 50, 200, 100), "YOU WON!"))
             {
-                SceneManager.LoadScene(0);
-                Time.timeScale = 1.0f;
+                RestartLevel();
+            }
+        }
+        if (showLossScreen)
+        {
+            if (GUI.Button(new Rect(Screen.width / 2 - 100, Screen.height / 2 - 50, 200, 100), "You loss..."))
+            {
+                RestartLevel();
             }
         }
     }
--- My Hero Born/Assets/Scripts/PlayerBehavior.cs
+++ My Hero Born/Assets/Scripts/PlayerBehavior.cs
@@ -1,5 +1,4 @@
-﻿                                                                                                                                                                                                                                                                                                               using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerBehavior : MonoBehaviour
@@ -18,21 +17,20 @@ public class PlayerBehavior : MonoBehaviour
     private float hInput;
     private Rigidbody _rb;
     private CapsuleCollider _col;
+    private GameBehavior _gameManager;
 
     void Start()
     {
         _rb = GetComponent<Rigidbody>();
-
         _col = GetComponent<CapsuleCollider>();
+        _gameManager = GameObject.Find("GameManager").GetComponent<GameBehavior>();
     }
-    
 
     void Update()
     {
         vInput = Input.GetAxis("Vertical") * moveSpeed;
 
         hInput = Input.GetAxis("Horizontal") * rotateSpeed;
-
         /*
         this.transform.Translate(Vector3.forward * vInput * Time.deltaTime);
         this.transform.Rotate(Vector3.up * hInput * Time.deltaTime);
@@ -40,18 +38,18 @@ public class PlayerBehavior : MonoBehaviour
     }
     void FixedUpdate()
     {
-        if(IsGrounded() && Input.GetKeyDown(KeyCode.Space))
-            {
-                _rb.AddForce(Vector3.up * jumpVelocity, ForceMode.Impulse);
-            }
+        if (IsGrounded() && Input.GetKeyDown(KeyCode.Space))
+        {
+            _rb.AddForce(Vector3.up * jumpVelocity, ForceMode.Impulse);
+        }
 
-            Vector3 rotation = Vector3.up * hInput;
+        Vector3 rotation = Vector3.up * hInput;
 
-            Quaternion angleRot = Quaternion.Euler(rotation * Time.fixedDeltaTime);
+        Quaternion angleRot = Quaternion.Euler(rotation * Time.fixedDeltaTime);
 
-            _rb.MovePosition(this.transform.position + this.transform.forward * vInput * Time.fixedDeltaTime);
+        _rb.MovePosition(this.transform.position + this.transform.forward * vInput * Time.fixedDeltaTime);
 
-            _rb.MoveRotation(_rb.rotation * angleRot);
+        _rb.MoveRotation(_rb.rotation * angleRot);
         if (Input.GetMouseButtonDown(0))
         {
             GameObject newBullet = Instantiate(bullet, this.transform.position + new Vector3(1, 0, 0), this.transform.rotation) as GameObject;
@@ -60,9 +58,8 @@ public class PlayerBehavior : MonoBehaviour
 
             bulletRB.velocity = this.transform.forward * bulletSpeed;
         }
-        
-    }
 
+    }
     private bool IsGrounded()
     {
         Vector3 capsuleBottom = new Vector3(_col.bounds.center.x, _col.bounds.min.y, _col.bounds.center.z);
@@ -70,5 +67,14 @@ public class PlayerBehavior : MonoBehaviour
         bool grounded = Physics.CheckCapsule(_col.bounds.center, capsuleBottom, distanceToGround, groundLayer, QueryTriggerInteraction.Ignore);
 
         return grounded;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Enemy")
+        {
+            Debug.Log("hp");
+            _gameManager.HP -= 1;
+        }
     }
 }
